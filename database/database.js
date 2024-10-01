5e8794f9f32615d2b3d4d1d3242d265f26295178
cuongvu3009 <cuongvu3009@gmail.com>
2024-10-01 14:05:51 +0300

    fix db connection

--- database/database.js
+++ database/database.js
@@ -1,14 +1,15 @@
 import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
 
-// Initialize connection to the PostgreSQL database
 const sql = postgres({
-  host: Deno.env.get("POSTGRES_HOST") || "dpg-crts42m8ii6s73al16tg-a",
-  port: Number(Deno.env.get("POSTGRES_PORT")) || 5432,
-  database: Deno.env.get("POSTGRES_DB") || "messages_nrk1",
-  username: Deno.env.get("POSTGRES_USER") || "admin",
-  password:
-    Deno.env.get("POSTGRES_PASSWORD") || "7vQd7nPmxi6adWhmtfXvFLWJfBH0cqRp",
+  host: "dpg-crts42m8ii6s73al16tg-a.frankfurt-postgres.render.com",
+  port: 5432,
+  database: "messages_nrk1",
+  username: "admin",
+  password: "7vQd7nPmxi6adWhmtfXvFLWJfBH0cqRp",
+  ssl: true, // Ensure SSL is enabled for secure connections
+  max: 10, // Maximum number of connections in the pool
+  idle_timeout: 10, // Connection will be closed after being idle for 10 seconds
+  connection_timeout: 5, // Will attempt to connect for 5 seconds before failing
 });
 
-// Export the sql instance
 export { sql };
