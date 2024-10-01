48f31014ee54b3c33afe76e580b31830ded85d8b
cuongvu3009 <cuongvu3009@gmail.com>
2024-10-01 12:46:36 +0300

    fix

--- database/database.js
+++ database/database.js
@@ -2,11 +2,12 @@ import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
 
 // Initialize connection to the PostgreSQL database
 const sql = postgres({
-  host: Deno.env.get("POSTGRES_HOST") || "localhost",
+  host: Deno.env.get("POSTGRES_HOST") || "dpg-crts42m8ii6s73al16tg-a",
   port: Number(Deno.env.get("POSTGRES_PORT")) || 5432,
-  database: Deno.env.get("POSTGRES_DB") || "messages",
-  username: Deno.env.get("POSTGRES_USER") || "postgres",
-  password: Deno.env.get("POSTGRES_PASSWORD") || "099730",
+  database: Deno.env.get("POSTGRES_DB") || "messages_nrk1",
+  username: Deno.env.get("POSTGRES_USER") || "admin",
+  password:
+    Deno.env.get("POSTGRES_PASSWORD") || "7vQd7nPmxi6adWhmtfXvFLWJfBH0cqRp",
 });
 
 // Export the sql instance
